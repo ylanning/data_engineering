81bdf62d404b768202371b11bbb4dbff4b11de02
Yanti.Lanning <yanti.lanning@virginmediao2.co.uk>
2026-03-06 11:22:02 +0000

    add github cicd configuration

--- dags/main.py
+++ dags/main.py
@@ -10,6 +10,7 @@ from api.video_stats import (
 
 from datawarehouse.data_warehouse import staging_table, core_table
 from dataquality.soda import yt_elt_data_quality
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 # Default Args
 default_args = {
@@ -30,13 +31,14 @@ default_args = {
 staging_schema = "staging"
 core_schema = "core"
 
+# DAG 1: import_json
 with DAG(
     dag_id="import_json",
     default_args=default_args,
     description="A DAG to generate a JSON file with raw data from the YouTube API",
     schedule="0 14 * * *",
     catchup=False,
-) as dag:
+) as dag_import_json:
 
     # Tasks
     playlist_id = get_playlist_id()
@@ -44,30 +46,44 @@ with DAG(
     extracted_stats = extract_video_stats(video_ids)
     save_to_json = save_stats_to_json(extracted_stats)
 
-    # Task dependencies
-    playlist_id >> video_ids >> extracted_stats >> save_to_json
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+        wait_for_completion=True,
+    )
 
+    # Task dependencies
+    playlist_id >> video_ids >> extracted_stats >> save_to_json >> trigger_update_db
+
+# DAG 2: update_db
 with DAG(
     dag_id="update_db",
     default_args=default_args,
     description="A DAG to update the staging and core tables in the Postgres database with the latest data from the YouTube API",
-    schedule="0 15 * * *",
+    schedule=None,
     catchup=False,
-) as dag:
+) as dag_update_db:
 
     # Tasks
     update_staging = staging_table()
     update_core = core_table()
 
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality_checks",
+        wait_for_completion=True,
+    )
+
     # Task dependencies
-    update_staging >> update_core
+    update_staging >> update_core >> trigger_data_quality
 
 
+# DAG 3: data_quality
 with DAG(
     dag_id="data_quality_checks",
     default_args=default_args,
     description="A DAG to perform data quality checks on the staging and core tables in the Postgres database",
-    schedule="0 16 * * *",
+    schedule=None,
     catchup=False,
 ) as dag:
 
